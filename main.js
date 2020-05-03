47cf9c603dcb8600ad59bb03629b297ee7e6ee96
Paul Saikko <paul.saikko@gmail.com>
2020-05-04 00:47:55 +0300

    Add centering force

--- main.js
+++ main.js
@@ -8,12 +8,19 @@ let boids = []
 
 function init() {
     for (let i = 0; i < n_boids; ++i) {
-        let x = Math.random() * W | 0;
-        let y = Math.random() * W | 0;
+        let x = Math.random() * W;
+        let y = Math.random() * W;
         let vx = Math.random() - 0.5;
         let vy = Math.random() - 0.5;
         boids.unshift({x:x, y:y, vx:vx, vy:vy});
     }
+}
+
+function unit_vector(from_x, from_y, to_x, to_y) {
+    let dx = to_x - from_x;
+    let dy = to_y - from_y;
+    let len = Math.sqrt(dx*dx + dy*dy);
+    return {x:dx/len, y:dy/len};
 }
 
 function update() {
@@ -27,10 +34,26 @@ function update() {
         ctx.fill();
     }
 
+    let centroid = {x:0, y:0};
+
+    // Update positions and compute center
     boids.forEach(boid => {
         boid.x += boid.vx;
         boid.y += boid.vy;
+        centroid.x += boid.x;
+        centroid.y += boid.y;
     });
+
+    centroid.x /= n_boids;
+    centroid.y /= n_boids;
+
+    // Add pull towards center
+    let pull_force = 0.1;
+    boids.forEach(boid => {
+        let dir = unit_vector(boid.x, boid.y, centroid.x, centroid.y);
+        boid.vx += dir.x * pull_force;
+        boid.vy += dir.y * pull_force;
+    })
 }
 
 init();
